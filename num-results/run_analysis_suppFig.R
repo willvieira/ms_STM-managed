cba2af4b4cf3574859b41ff57b2c31daf3c68aaa
Willian Vieira <willian.vieira@usherbrooke.ca>
2019-06-19 21:46:25 -0400

    update supplementary fig with env1 boreal and mixed together

--- num-results/run_analysis_suppFig.R
+++ num-results/run_analysis_suppFig.R
@@ -58,65 +58,98 @@ solve_summary <- function(env1a, RCP, RCPgrowth, managPractices = c(1, 0, 0, 0))
 
 # get dat for each management practices and then plot it
 
+# practices
+practices <- c('Plantation', 'Harvest', 'Thinning', 'Enrichment')
+
 # two conditions of fixed environment
-ev1a <- c(0, -1)
+ev1a <- c(-1, 0)
 
-for(ENV1a in ev1a) {
+count = 1
+for(mg in practices) {
 
-  print(paste("Supplementary figure", which(ENV1a == ev1a), "of", length(ev1a)))
+  manag <- c(0, 0, 0, 0)
+  manag[which(mg == practices)] <- 1
 
-  print("simulation 1 of 4")
-  dat_Plantation <- solve_summary(env1a = ENV1a, RCP = 4.5, RCPgrowth = 'linear', managPractices = c(1, 0, 0, 0))
-  print("simulation 2 of 4")
-  dat_Harvest <- solve_summary(env1a = ENV1a, RCP = 4.5, RCPgrowth = 'linear', managPractices = c(0, 1, 0, 0))
-  print("simulation 3 of 4")
-  dat_Thinning <- solve_summary(env1a = ENV1a, RCP = 4.5, RCPgrowth = 'linear', managPractices = c(0, 0, 1, 0))
-  print("simulation 4 of 4")
-  dat_Enrichment <- solve_summary(env1a = ENV1a, RCP = 4.5, RCPgrowth = 'linear', managPractices = c(0, 0, 0, 1))
+  for(ENV1a in ev1a) {
+    print(paste0("simulation ", count, " of ", length(practices) * length(ev1a)))
+    count = count + 1
+
+    # solve simulation
+    assign(paste0('dat_', mg, '_', ENV1a), solve_summary(env1a = ENV1a, RCP = 4.5, RCPgrowth = 'linear', managPractices = manag))
+  }
+}
 
 
+#############################
+# Plot
+#############################
 
-  #############################
-  # Plot
-  #############################
+metrics <- c('Exposure', 'Asymptotic resilience', 'Sensitivity', 'Initial resilience', 'Cumulative state changes')
+leg <- letters[1:(length(metrics) * 2)]
+leg <- setNames(paste0('(', leg, ')'), do.call(paste0, expand.grid(metrics, ev1a)))
 
-  practices <- c('Plantation', 'Harvest', 'Thinning', 'Enrichment')
-  metrics <- c('Exposure', 'Asymptotic resilience', 'Sensitivity', 'Initial resilience', 'Cumulative state changes')
-  leg <- letters[1:length(metrics)]
-  leg <- setNames(paste0('(', leg, ')'), metrics)
-
-  # ylim for each variable
-  nR <- nrow(dat_Plantation)
+# ylim for each variable for each ENV1
+nR <- nrow(dat_Plantation_0)
+for(env1a in ev1a) {
   for(mt in metrics)
   {
     vars <- rep(NA, length(practices) * nR)
     count = 0
     for(mg in practices)
     {
-      vars[(nR * count + 1):(nR * (count + 1))] <- get(paste0('dat_', mg))[, mt]
+      vars[(nR * count + 1):(nR * (count + 1))] <- get(paste0('dat_', mg, '_', env1a))[, mt]
       count <- count + 1
     }
-    assign(paste0('ylim', mt), c(min(vars), max(vars)))
+    assign(paste0('ylim', mt, env1a), c(min(vars), max(vars)))
   }
+}
 
-  print(paste('Plot supplementary figure', which(ENV1a == ev1a)))
-  pdf(file = paste0('manuscript/img/supp-num-result_env1a_', ENV1a, '.pdf'), width = 6.4)
-  par(mfrow = c(3, 2), mar = c(2.1, 2.5, 1, 0.5), mgp = c(1.4, 0.2, 0), tck = -.008, cex = 0.8)
-  for(mt in metrics)
-  {
-    plot(0, pch = '', xlim = c(0, 1), ylim = get(paste0('ylim', mt)), xlab = '', ylab = mt, cex.lab = 1.1)
+# ylim for each variable independent of env1a
+nR <- nrow(dat_Plantation_0)
+for(mt in metrics)
+{
+  vars <- rep(NA, length(practices) * length(ev1a) * nR)
+  count = 0
+  for(env1a in ev1a) {
     for(mg in practices)
     {
-      points(get(paste0('dat_', mg))[, c('managInt', mt)], type = 'l', lwd = 1.2, lty = which(mg == practices))
+      vars[(nR * count + 1):(nR * (count + 1))] <- get(paste0('dat_', mg, '_', env1a))[, mt]
+      count <- count + 1
     }
-    legend(par('usr')[1] - (par('usr')[2]-par('usr')[1])*0.06, par('usr')[4], legend = leg[mt], bty = 'n', cex = 1.2)
+  }
+  assign(paste0('ylim', mt), c(min(vars), max(vars)))
+}
+
+
+
+print('Plot supplementary figure')
+
+
+pdf(file = 'manuscript/img/supp-num-result.pdf', width = 6.4, height = 10)
+par(mfcol = c(5, 2), mar = c(1, 1.5, 0.5, 0.8), oma = c(1.5, 1, 0.5, 0), mgp = c(1.4, 0.2, 0), tck = -.008, cex = 0.8)
+for(env1a in ev1a) {
+  for(mt in metrics)
+  {
+    # empty plot
+    plot(0, pch = '', xaxt = 'n', xlim = c(0, 1), ylim = get(paste0('ylim', mt)), xlab = '', ylab = '', cex.lab = 1.1)
+    # xaxis
+    axis(1, labels = ifelse(mt == 'Cumulative state changes', T, F))
+    # main for env1a
+    if(mt == 'Exposure') mtext(paste0('Latitude = ', env1a, ' (', ifelse(env1a == ev1a[1], 'Boreal', 'Mixed'), ')'), 3, line = 0, cex = .92)
+    # ylab
+    if(env1a == ev1a[1]) mtext(mt, 2, line = 1.3, cex = 0.92)
+
+    # lines of each practices
+    for(mg in practices)
+    {
+      points(get(paste0('dat_', mg, '_', env1a))[, c('managInt', mt)], type = 'l', lwd = 1.2, lty = which(mg == practices))
+    }
+    legend(par('usr')[1] - (par('usr')[2]-par('usr')[1])*0.06, par('usr')[4], legend = leg[paste0(mt, env1a)], bty = 'n', cex = 1.2)
+
+    # legend
+    if(env1a == ev1a[2] & mt == 'Sensitivity') legend('topright', legend = c('Plantation', 'Harvest', 'Thinning', 'Enrichment'), lty = 1:4, bty = 'n', cex = 1)
   }
 
-  # empty plot for legend
-  plot(1, type="n", axes=F, xlab="", ylab="")
-  legend('center', legend = c('Plantation', 'Harvest', 'Thinning', 'Enrichment'), lty = 1:4, bty = 'n', cex = 1)
-  # text
-  mtext("Management intensity", 1, line = -1, cex = .92, outer = TRUE)
-
-  dev.off()
 }
+mtext("Management intensity", 1, line = 0.2, cex = .92, outer = TRUE)
+dev.off()
