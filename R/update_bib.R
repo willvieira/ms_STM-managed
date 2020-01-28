ca327703a4ff52fb1fb1f9ebc291f2c14b1acae4
Willian <willian.vieira@usherbrooke.ca>
2020-01-28 10:46:01 -0500

    explicit dep

--- R/update_bib.R
+++ R/update_bib.R
@@ -15,7 +15,7 @@ library(RefManageR)
   # Set Master BibTeX file location
   tmp <- tempfile()
   download.file(url = 'https://doc.ielab.usherbrooke.ca/index.php/s/Y8YhGJ8k2lyqgwh/download', destfile = tmp, quiet = TRUE)
-  refs <- suppressWarnings(suppressMessages(ReadBib(file = tmp)))
+  refs <- suppressWarnings(suppressMessages(RefManageR::ReadBib(file = tmp)))
 
   # Citation entries to keep, all others are skipped
   keep <- c('author', 'title', 'journal', 'year', 'volume', 'number',
@@ -111,7 +111,7 @@ library(RefManageR)
 
     # save file
     WriteBib(newBib, file = newbibfile)
-    
+
   }else { # Othewise just write a bibfile with the references
 
     cat('Creating bib file:', newbibfile, '\n')
