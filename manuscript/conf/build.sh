37890a137e5a7fcb05d2cd1da37516422918b485
Willian <willian.vieira@usherbrooke.ca>
2021-12-06 15:48:16 -0500

    check if download is wowrking

--- manuscript/conf/build.sh
+++ manuscript/conf/build.sh
@@ -26,8 +26,16 @@
 
 
 # Load metadata.yml using a bash script from: https://github.com/jasperes/bash-yaml
-source <(curl -s https://raw.githubusercontent.com/jasperes/bash-yaml/master/script/yaml.sh)
+curl -s https://raw.githubusercontent.com/jasperes/bash-yaml/master/script/yaml.sh -o ./load_yaml.sh
+FILE=./load_yaml.sh
+if [ -f "$FILE" ]; then
+    echo "$FILE exists."
+else 
+    echo "$FILE does not exist."
+fi
+source ./load_yaml.sh
 create_variables $3
+rm ./load_yaml.sh
 
 
 # Folder to save manuscript outputs
